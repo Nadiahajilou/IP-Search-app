138d3fd40102130a97e7e2476d28dff25f0dad52
Nadia Hajiloo <nadiahajiloo5@gmail.com>
2024-08-02 13:39:22 +0330

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import IPSearch from "./pages/IPSearch";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <>
-        <Routes>
+      <Routes>
         <Route path="/" element={<Navigate to="/auth-page" />} />
         <Route path="/auth-page" element={<AuthPage />} />
         <Route path="/IP-page" element={<IPSearch />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </>
   );
 }
-
 
 export default App;
